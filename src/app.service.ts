415644b527e07dddc18e0f52e5b8fd181c665aef
hichembenaliabdallah <hichem.benali101@gmail.com>
2023-09-02 06:37:50 +0100

    uno

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
+import { config } from 'dotenv';
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
+config();
+export interface data {
+  data: object;
+  port: string;
+}
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello !';
+  }
+  async sayPort(): Promise<data> {
+    const { data } = await axios.get(
+      'https://jsonplaceholder.typicode.com/posts',
+    );
+
+    return {
+      data: data[1],
+      port: process.env.PORT,
+    };
   }
 }
